1c4aa30b74c0bc3e0fbb2298222f01b305803587
long.nn@neo-lab.vn <long.nn@neo-lab.vn>
2018-10-26 16:24:57 +0700

    add code

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import Note from './Note';
 import NoteForm from './NoteForm';
 import {connect} from 'react-redux';
 
 class List extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     mang: ['Android', 'iOS', 'NodeJs']
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   mang: ['Android', 'iOS', 'NodeJs']
+    // };
+    // this.getListData = this.getListData.bind(this);
+    this.getListData();
+  }
+
+   getListData() {
+     console.log('asdasdasdasdasdasd');
+    var {dispatch} = this.props;
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        if (res && res.data) {
+          res.data.map((item) => {
+            let itemName = item.name;
+            console.log(itemName);
+            dispatch({type: 'ADD_ITEM', item: item.name});
+          });
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
 
   remove(index) {
     // this.state.mang.splice(index, 1);
--- src/reducers/mangReducer.jsx
+++ src/reducers/mangReducer.jsx
@@ -1,6 +1,39 @@
-import {userData} from 'dumbData';
+import axios from 'axios';
+
+var userData = ['AAA', 'CCC'];
+
+// axios.get('https://jsonplaceholder.typicode.com/users')
+//   .then(res => {
+//     if (res && res.data) {
+//       userData = [];
+//       res.data.map((e) => {
+//         userData.push(e.name);
+//       });
+//     }
+//     console.log(userData);
+//     return userData;
+//   });
+//
+// let arrayData = [1, 2, 3];
+//
+// let newArray = arrayData.map(e =>
+//   e*2
+// )
+
+// let getFunction = (num) => {
+//   if (num > 0) {
+//     return () => console.log('so duong')
+//   } else {
+//     return () => console.log('so am')
+//   }
+// }
+//
+// getFunction(5)();
+
+// console.log(newArray);
 
 const mangReducer = (state = userData, action) => {
+  // console.log('Reducer done...');
   switch (action.type) {
     case 'ADD_ITEM':
       return [...state, action.item];
